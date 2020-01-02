09933ec6cc982c03a49b76b4c53453158b8e2332
hucailong <474121845@qq.com>
2020-01-02 18:45:58 +0800

    debug wechat_Cache

--- app/Tools/Wechat.php
+++ app/Tools/Wechat.php
@@ -1,13 +1,17 @@
 <?php
 
 namespace App\Tools;
-
+use Illuminate\Support\Facades\Cache;
 
 class Wechat
 {
         public  static function getAccessToken(){
-            $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".env('WECHAT_APPID')."&secret=".env('WECHAT_APP_SECRET')."";
-            $access_token = json_decode(file_get_contents($url),true)['access_token'];
+            $access_token = Cache::get('access_token');
+            if (empty($access_token)) {
+                $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".env('WECHAT_APPID')."&secret=".env('WECHAT_APP_SECRET')."";
+                $access_token = json_decode(file_get_contents($url),true)['access_token'];
+                Cache::put('access_token',$access_token);
+            }
             return $access_token;
         }
 
@@ -17,6 +21,7 @@ class Wechat
      */
         public  static function getUserInfo($xml_obj){
             $access_token = self::getAccessToken();
+
             $user_url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=".$access_token."&openid=".$xml_obj->FromUserName."&lang=zh_CN";
             return file_get_contents($user_url);
         }
